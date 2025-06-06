43c3c3e7bcdb79526ae2bfb970e6bfc9d3b3aee6
adandoang <akhdanjzif@gmail.com>
2025-06-06 11:53:24 +0700

    membuta buyer profile screen

---
+++ lib/presentation/buyer/profile/buyer_profile_screen.dart
@@ -0,0 +1,51 @@
+import 'package:canary_project/presentation/buyer/profile/bloc/profile_buyer_bloc.dart';
+import 'package:canary_project/presentation/buyer/profile/widget/profile_view_buyer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BuyerProfilePage extends StatefulWidget {
+  const BuyerProfilePage({super.key});
+
+  @override
+  State<BuyerProfilePage> createState() => _BuyerProfilePageState();
+}
+
+class _BuyerProfilePageState extends State<BuyerProfilePage> {
+  @override
+  initState() {
+    super.initState();
+    context.read<ProfileBuyerBloc>().add(GetProfileBuyerEvent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Profil Pembeli")),
+      body: BlocListener<ProfileBuyerBloc, ProfileBuyerState>(
+        listener: (context, state) {
+          if (state is ProfileBuyerAdded) {
+            context.read<ProfileBuyerBloc>().add(GetProfileBuyerEvent());
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text("Profil berhasil ditambahkan")),
+            );
+          }
+        },
+        child: BlocBuilder<ProfileBuyerBloc, ProfileBuyerState>(
+          builder: (context, state) {
+            if (state is ProfileBuyerLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (state is ProfileBuyerLoaded &&
+                state.profile.data.name.isNotEmpty) {
+              final profile = state.profile.data;
+              return ProfileViewBuyer(profile: profile);
+            }
+
+            return ProfileBuyerInputForm();
+          },
+        ),
+      ),
+    );
+  }
+}
